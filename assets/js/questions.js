152fb862f801c054ce6e76f7bb224875f0e5ed87
Jason-fos <jason.fos2812@outlook.com>
2023-03-05 14:42:44 +0000

    add quiz questions to questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,201 @@
+let questions = [{
+        //Question 1
+        question: "What boxing weight class is heaviest?",
+        a: "Fly weight",
+        b: "Bantam weight",
+        c: "Feather weight",
+        d: "Catch weight",
+        answer: "Feather weight"
+    },
+
+    {
+        //Question 2
+        question: "How old was George Foreman when he became the oldest heavyweight champion?",
+        a: "45",
+        b: "52",
+        c: "44",
+        d: "48",
+        answer: "45"
+    },
+
+    {
+        //Question 3
+        question: "Which boxing promoter's favourite expression is 'Only in America'?",
+        a: "Eddie Hern",
+        b: "Don King",
+        c: "Frank Warren",
+        d: "Bob Arum",
+        answer: "Don King"
+    },
+
+    {
+        //Question 4
+        question: "How many olymic medals has Tyson Fury won?",
+        a: "One",
+        b: "Two",
+        c: "Three",
+        d: "None",
+        answer: "None"
+    },
+
+    {
+        //Question 5
+        question: "American boxer James J. Braddock was given which nickname inspired by a popular fairytale story?",
+        a: "Rapunzel",
+        b: "Cinderella Man",
+        c: "Gingerbread Man",
+        d: "Sleeping Beauty",
+        answer: "Cinderella Man"
+    },
+
+    {
+        //Question 6
+        question: "In which round did Anthony Joshua defeat Wladimir Klitschko in their heavyweight showdown in 2017?",
+        a: "2nd",
+        b: "5th",
+        c: "9th",
+        d: "11th",
+        answer: "11th"
+    },
+
+    {
+        //Question 7
+        question: "Who did Deontay Wilder defeat in November 2019 – his last victory before his rematch with Tyson Fury?",
+        a: "Anthony Joshua",
+        b: "Dillian Whyte",
+        c: "Luis Ortiz",
+        d: "Jarell Miller",
+        answer: "Luis Ortiz"
+    },
+
+    {
+        //Question 8
+        question: "How many times has Anthony Joshua fought outside of the UK?",
+        a: "Once",
+        b: "Three times",
+        c: "Five times",
+        d: "Twice",
+        answer: "Twice"
+    },
+
+    {
+        //Question 9
+        question: "In which year was Floyd Mayweather's last professional fight prior to his comeback showdown with Conor McGregor?",
+        a: "2015",
+        b: "2018",
+        c: "2016",
+        d: "2014",
+        answer: "2015"
+    },
+
+    {
+        //Question 10
+        question: "Lennox Lewis was defeated by Hasim Rahman in 2001. What excuse did his promoter give for his lack of focus?",
+        a: "He was feeling under the weather",
+        b: "He was filming Ocean's Eleven at the time",
+        c: "He had some sort of locker room disruption before the fight",
+        d: "He was distracted by a friend in the crowd",
+        answer: "He was filming Ocean's Eleven at the time"
+    },
+
+    {
+        //Question 11
+        question: "Which fighters starred in the 1971 bout that would later be known as 'Fight of the Century'?",
+        a: "Sugar Ray Leonard, Joe Frazier",
+        b: "George Foreman, Muhammad Ali",
+        c: "Larry Holmes, Sonny Liston",
+        d: "Muhammad Ali, Joe Frazier",
+        answer: "Muhammad Ali, Joe Frazier"
+    },
+
+    {
+        //Question 12
+        question: "Which fighter starred as 'Pretty' Ricky Conlan in Rocky spin-off film Creed?",
+        a: "Olyksander Usyk",
+        b: "George Groves",
+        c: "Tony Bellew",
+        d: "Deontay Wilder",
+        answer: "Tony Bellew"
+    },
+
+    {
+        //Question 13
+        question: "Who was David Haye's last opponent in 2012 before taking a four-year break from the sport?",
+        a: "Dereck Chisora",
+        b: "Vasyl Lomachenko",
+        c: "Luke Campbell",
+        d: "Tony Bellew",
+        answer: "Dereck Chisora"
+    },
+
+    {
+        //Question 14
+        question: "Which super-middleweight fighter retired in 2008 with 46 wins, no draws, no defeats?",
+        a: "Andre Ward",
+        b: "Sven Otke",
+        c: "Joe Calzaghe",
+        d: "Carl Frosch",
+        answer: "Joe Calzaghe"
+    },
+
+    {
+        //Question 15
+        question: "Which fighter knocked out Muhammad Ali in controversial circumstances towards the end of his career? (Ali was suffering from early symptoms of Parkinson's disease at the time)",
+        a: "Sonny Liston",
+        b: "Bernard Hopkins",
+        c: "Larry Holmes",
+        d: "Joe Frazier",
+        answer: "Larry Holmes"
+    },
+
+    {
+        //Question 16
+        question: "What is the northern-most UK city Anthony Joshua has fought in?",
+        a: "Sheffield",
+        b: "Glasgow",
+        c: "Manchester",
+        d: "Newcastle upon Tyne",
+        answer: "Glasgow"
+    },
+
+    {
+        //Question 17
+        question: "Who was Mike Tyson's last opponent in professional boxing?",
+        a: "Lennox Lewis",
+        b: "Evander Holyfield",
+        c: "Kevin McBride",
+        d: "Buster Douglas",
+        answer: "Kevin McBride"
+    },
+
+    {
+        //Question 18
+        question: "In which German city did Tyson Fury defeat Wladimir Klitschko in 2015?",
+        a: "Dusseldorf",
+        b: "Berlin",
+        c: "Frankfurt",
+        d: "Stuttgart",
+        answer: "Dusseldorf"
+    },
+
+    {
+        //Question 19
+        question: "Canelo Alvarez signed a five-year £278 million contract with which sport broadcaster?",
+        a: "ESPN",
+        b: "Showtime Boxing",
+        c: "Sky Sports Boxing",
+        d: "DAZN",
+        answer: "DAZN"
+    },
+
+    {
+        //Question 20
+        question: "Which boxer was crowned the youngest ever heavyweight world champion at 20yrs old?",
+        a: "Sonny Liston",
+        b: "Muhammad Ali",
+        c: "Mike Tyson",
+        d: "Joe Frazier",
+        answer: "Mike Tyson"
+    },
+
+]
