c7b5697ba75916ff5d26d66255b1b05005bba426
Jason-fos <jason.fos2812@outlook.com>
2023-03-12 07:28:05 +0000

    add ; to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 checkAnswer();
             } else if (this.getAttribute("id") === "nextQ-button") {
                 runQuiz();
-                document.getElementById("right-wrong-text").nextElementSibling = selectText;
+                document.getElementById("right-wrong-text").innerHTML = selectText;
                 document.getElementById("right-wrong-text").style.backgroundColor = "rgba(0, 0, 255, 0.701)";
             }
         });
